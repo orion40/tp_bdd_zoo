3b1474a68671499be85ea81e32de1418a41ee8c0
David Bozon <bozond@im2ag-mandelbrot.e.ujf-grenoble.fr>
2017-10-21 18:15:25 +0200

    Add triggers for diseases

--- script.sql
+++ script.sql
@@ -72,71 +72,88 @@ create table LesHistoiresAff (
 	constraint LesHistoiresAff_C1 primary key (dateFin, noCage, nomE)
 );
 
+    insert into LesCages values (11 ,  'fauve'           , 10 );
+    insert into LesCages values (1     , 'fosse'         , 1 );
+    insert into LesCages values (2     , 'aquarium'      , 1 );
+    insert into LesCages values (3     , 'petits oiseaux'        , 2 );
+    insert into LesCages values (4     , 'grand aquarium'        , 1 );
+    insert into LesCages values (12     , 'fauve'           , 10);
+
+
+
+    insert into LesEmployes values ('Verdier'  ,       'Noumea' );
+    insert into LesEmployes values ('Spinnard'  ,       'Sartene' );
+    insert into LesEmployes values ('Labbe' ,    'Calvi' );
+    insert into LesEmployes values ('Lachaize' ,       'Pointe a Pitre' );
+    insert into LesEmployes values ('Desmoulins'  , 'Ushuaia' );
+    insert into LesEmployes values ('Jouanot'   , 'Papeete' );
+
+    insert into LesSpecialites values ('Verdier'  ,       'fauve' );
+    insert into LesSpecialites values ('Spinnard'  ,       'fauve' );
+    insert into LesSpecialites values ('Labbe' ,    'fauve' );
+    insert into LesSpecialites values ('Lachaize' ,       'fauve' );
+    insert into LesSpecialites values ('Lachaize' ,       'fosse' );
+    insert into LesSpecialites values ('Lachaize' ,       'petis oiseaux' );
+    insert into LesSpecialites values ('Desmoulins'  , 'fauve' );
+    insert into LesSpecialites values ('Desmoulins'  , 'fosse' );
+    insert into LesSpecialites values ('Desmoulins'  , 'petits oiseaux' );
+    insert into LesSpecialites values ('Jouanot'   , 'fosse' );
+    insert into LesSpecialites values ('Jouanot' ,       'aquarium' );
+    insert into LesSpecialites values ('Jouanot' ,       'gand aquarium' );
+
+
+    insert into LesResponsables values (10      ,       'Verdier' );
+    insert into LesResponsables values (1       ,       'Jouanot' );
+    insert into LesResponsables values (2       ,       'Desmoulins');
+
+
+    insert into LesGardiens values (11      ,       'Lachaize' );
+    insert into LesGardiens values (12      ,       'Spinnard' );
+    insert into LesGardiens values (12      ,       'Labbe' );
+    insert into LesGardiens values (11      ,       'Spinnard' );
+    insert into LesGardiens values (11      ,       'Labbe' );
+    insert into LesGardiens values (1       ,       'Lachaize' );
+    insert into LesGardiens values (3       ,       'Lachaize' );
+    insert into LesGardiens values (12      ,       'Lachaize' );
+
+    insert into LesAnimaux values ('Charly', 'male',   'lion', 'fauve',  'Kenya',  2010,   12,2);
+    insert into LesAnimaux values ('Arthur', 'male',   'ours', 'fosse',  'France', 2000,   1,0 );
+    insert into LesAnimaux values ('Chloe',  'femelle', 'pie', 'petits oiseaux' ,  'France', 2011,   3,1 );
+    insert into LesAnimaux values ('Milou',  'male' ,  'leopard', 'fauve', 'France', 2013,  11,1 );
+    insert into LesAnimaux values ('Tintin', 'male' , 'leopard', 'fauve', 'France', 2013,    11,0 );
+    insert into LesAnimaux values ('Charlotte', 'femelle', 'lion',  'fauve',  'Kenya',  2012,      12,0 );
+
+    insert into LesMaladies values ('Charly'        , 'rage de dents' );
+    insert into LesMaladies values ('Charly'        , 'grippe' );
+    insert into LesMaladies values ('Milou'         , 'angine' );
+    insert into LesMaladies values ('Chloe'         , 'grippe' );
+
 -- TODO: inc/dec les maladies de chaque animal à chaque modif
 -- TODO: Vérifier la fontion animal = fonction cage
 -- TODO: Vérifier la présence d'un gardien
+
 CREATE OR REPLACE TRIGGER ajout_maladie_animal
 AFTER INSERT ON LesMaladies
-DECLARE NouveauNomAnimal VARCHAR(20);
-BEGIN 
-    NouveauNomAnimal := :NEW.nomA;
-    LesAnimaux nb_maladies = nb_maladies + 1 LesAnimaux
-    WHERE nomA = NouveauNomAnimal;
-END;
-/
+FOR EACH ROW
+    BEGIN
+        UPDATE LesAnimaux SET nb_maladies = nb_maladies + 1 WHERE nomA = :NEW.nomA;
+    END;
+    /
 
-insert into LesCages values (11 ,  'fauve'           , 10 );
-insert into LesCages values (1     , 'fosse'         , 1 );
-insert into LesCages values (2     , 'aquarium'      , 1 );
-insert into LesCages values (3     , 'petits oiseaux'        , 2 );
-insert into LesCages values (4     , 'grand aquarium'        , 1 );
-insert into LesCages values (12     , 'fauve'           , 10);
-
+    CREATE OR REPLACE TRIGGER suppr_maladie_animal
+    AFTER DELETE ON LesMaladies
+    FOR EACH ROW
+        BEGIN
+            UPDATE LesAnimaux SET nb_maladies = nb_maladies - 1 WHERE nomA = :OLD.nomA;
+        END;
+        /
 
 
-insert into LesEmployes values ('Verdier'  ,       'Noumea' );
-insert into LesEmployes values ('Spinnard'  ,       'Sartene' );
-insert into LesEmployes values ('Labbe' ,    'Calvi' );
-insert into LesEmployes values ('Lachaize' ,       'Pointe a Pitre' );
-insert into LesEmployes values ('Desmoulins'  , 'Ushuaia' );
-insert into LesEmployes values ('Jouanot'   , 'Papeete' );
-
-insert into LesSpecialites values ('Verdier'  ,       'fauve' );
-insert into LesSpecialites values ('Spinnard'  ,       'fauve' );
-insert into LesSpecialites values ('Labbe' ,    'fauve' );
-insert into LesSpecialites values ('Lachaize' ,       'fauve' );
-insert into LesSpecialites values ('Lachaize' ,       'fosse' );
-insert into LesSpecialites values ('Lachaize' ,       'petis oiseaux' );
-insert into LesSpecialites values ('Desmoulins'  , 'fauve' );
-insert into LesSpecialites values ('Desmoulins'  , 'fosse' );
-insert into LesSpecialites values ('Desmoulins'  , 'petits oiseaux' );
-insert into LesSpecialites values ('Jouanot'   , 'fosse' );
-insert into LesSpecialites values ('Jouanot' ,       'aquarium' );
-insert into LesSpecialites values ('Jouanot' ,       'gand aquarium' );
-
-
-insert into LesResponsables values (10      ,       'Verdier' );
-insert into LesResponsables values (1       ,       'Jouanot' );
-insert into LesResponsables values (2       ,       'Desmoulins');
-
-
-insert into LesGardiens values (11      ,       'Lachaize' );
-insert into LesGardiens values (12      ,       'Spinnard' );
-insert into LesGardiens values (12      ,       'Labbe' );
-insert into LesGardiens values (11      ,       'Spinnard' );
-insert into LesGardiens values (11      ,       'Labbe' );
-insert into LesGardiens values (1       ,       'Lachaize' );
-insert into LesGardiens values (3       ,       'Lachaize' );
-insert into LesGardiens values (12      ,       'Lachaize' );
-
-insert into LesAnimaux values ('Charly', 'male',   'lion', 'fauve',  'Kenya',  2010,   12,2);
-insert into LesAnimaux values ('Arthur', 'male',   'ours', 'fosse',  'France', 2000,   1,0 );
-insert into LesAnimaux values ('Chloe',  'femelle', 'pie', 'petits oiseaux' ,  'France', 2011,   3,1 );
-insert into LesAnimaux values ('Milou',  'male' ,  'leopard', 'fauve', 'France', 2013,  11,1 );
-insert into LesAnimaux values ('Tintin', 'male' , 'leopard', 'fauve', 'France', 2013,    11,0 );
-insert into LesAnimaux values ('Charlotte', 'femelle', 'lion',  'fauve',  'Kenya',  2012,      12,0 );
-
-insert into LesMaladies values ('Charly'        , 'rage de dents' );
-insert into LesMaladies values ('Charly'        , 'grippe' );
-insert into LesMaladies values ('Milou'         , 'angine' );
-insert into LesMaladies values ('Chloe'         , 'grippe' );
+         CREATE OR REPLACE TRIGGER update_maladie_animal
+         AFTER UPDATE ON LesMaladies
+         FOR EACH ROW
+             BEGIN
+                 UPDATE LesAnimaux SET nb_maladies = nb_maladies + 1 WHERE nomA = :NEW.nomA;
+                 UPDATE LesAnimaux SET nb_maladies = nb_maladies - 1 WHERE nomA = :OLD.nomA;
+             END;
+             /
